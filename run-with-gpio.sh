7393b60c17d9e17f1e90887f0937b63a5d39eb29
tloxipeuhca <tloxipeuhca@gmail.com>
2020-08-23 15:56:02 +0100

    Add docker run scripts

---
+++ run-with-gpio.sh
@@ -0,0 +1,15 @@
+sudo docker run --privileged \
+        -d \
+        --network=host \
+        --device=/dev/ttyAMA0 \
+        --device=/dev/ttyUSB0 \
+        --device=/dev/vchiq \
+        -e LD_LIBRARY_PATH=/opt/vc/lib  \
+        -v /opt/vc:/opt/vc:ro \
+        -v /etc/localtime:/etc/localtime \
+        -v $HOME/docker-data/domoticz/domoticz.db:/root/domoticz/domoticz.db:rw \
+        -v $HOME/docker-data/domoticz/domoticz.pem:/root/domoticz/domoticz.pem:rw \
+        -v $HOME/docker-data/domoticz/domoticz.log:/var/log/domoticz.log:rw \
+        --name domoticz \
+        --restart=always \
+        tloxipeuhca/rpi-domoticz-docker
